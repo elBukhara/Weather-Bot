959c95885deb023c5de0cba001f2faff5f99ea87
elBukhara <shakhzodgiazov@gmail.com>
2024-08-14 15:30:44 +0300

    prompts are in russian now

--- app/weather.py
+++ app/weather.py
@@ -1,6 +1,7 @@
 import os
-import pyowm
 import math
+import pyowm
+from pyowm.utils.config import get_default_config
 from dotenv import find_dotenv, load_dotenv
 
 dotenv_path = find_dotenv()
@@ -9,7 +10,9 @@ load_dotenv(dotenv_path)
 WEATHER_TOKEN = os.getenv('WEATHER_TOKEN')
 
 def return_data(city):
-    owm = pyowm.OWM(WEATHER_TOKEN)
+    config_dict = get_default_config()
+    config_dict['language'] = 'ru'
+    owm = pyowm.OWM(WEATHER_TOKEN, config_dict)
     mgr = owm.weather_manager()
     
     try:
@@ -27,19 +30,19 @@ def return_data(city):
         }
 
         responses = {
-            'city': f"City: {data['city']}",
-            'status': f"Current Status: {data['status']}.",
-            'temperature': f"Temperature: {math.ceil(data['temperature']['temp'])} ℃",
-            'wind': f"Wind speed: {math.ceil(data['wind'])} meter/per second",
-            'humidity': f"Hummidity: {data['humidity']}",
-            'high-temperature': f"Highest temperature: {math.ceil(data['temperature']['temp_max'])} ℃",
-            'min-temperature': f"Minimum temperature: {math.ceil(data['temperature']['temp_min'])} ℃",
-            'feels_like': f"Feels Like: {math.ceil(data['temperature']['feels_like'])} ℃"
+            'city': f"Город: {data['city']}",
+            'status': f"Текущий статус: {data['status']}.",
+            'temperature': f"Температура: {math.ceil(data['temperature']['temp'])} ℃",
+            'wind': f"Скорость ветра: {math.ceil(data['wind'])} метр/в секунду",
+            'humidity': f"Влажность: {data['humidity']}%",
+            'high-temperature': f"Макс. Температура: {math.ceil(data['temperature']['temp_max'])} ℃",
+            'min-temperature': f"Мин. Температура: {math.ceil(data['temperature']['temp_min'])} ℃",
+            'feels_like': f"Ощущается как: {math.ceil(data['temperature']['feels_like'])} ℃"
         }
 
         result = ""
         for key, value in responses.items():
-            result += f"{value}\n"
+            result += f"{value}\n" 
         return result.strip()
     except:
-        return f"Something went wrong! Perhaps the city '{city}' does not exist?\nEnter a valid city."
+        return f"Что-то пошло не так! Возможно город '{city}' не существует?\nНапишите действительный город."
